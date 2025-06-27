7652b060ddcfa2f607c8832a761cb76fd82fe473
AghaSKhan <agha.khan@surprisinggames.com>
2025-06-26 23:41:28 -0700

    Added  Spring Arm and camera

--- Source/Warrior/Public/Characters/WarriorHeroCharacter.h
+++ Source/Warrior/Public/Characters/WarriorHeroCharacter.h
@@ -6,6 +6,9 @@
 #include "Characters/WarriorBaseCharacter.h"
 #include "WarriorHeroCharacter.generated.h"
 
+//class USpringArmComponent;
+// class UCameraComponent;
+
 /**
  * 
  */
@@ -13,5 +16,22 @@ UCLASS()
 class WARRIOR_API AWarriorHeroCharacter : public AWarriorBaseCharacter
 {
 	GENERATED_BODY()
-	
+
+public:
+	AWarriorHeroCharacter();
+protected:
+	virtual void BeginPlay() override;
+
+private:
+
+#pragma region Components
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* FollowCamera;
+
+#pragma endregion
+		
 };
